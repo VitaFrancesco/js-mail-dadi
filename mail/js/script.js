659dfe7c0b7a51aebd5425c48317eaa3a76d9be8
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-17 18:47:46 +0200

    FIX: userMail variable

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,13 +1,13 @@
 const mailList = ['vitafrancesco00@gmail.com', 'romelosurdo@gmail.com', 'stresaantonio@yahoo.it', 'rossimario@gmail.com', 'verdifilippo@gmail.com']
 console.log(mailList)
 
-const usermail = prompt('Inserisci la tua mail')
+const userMail = prompt('Inserisci la tua mail')
 // mailList.includes(usermail) ? alert(`Bentornato ${usermail}`) : alert(`Non sei registrato!`) //${usermail} desideri registrarti? (implementare poi)
 
 let  message = 'Non sei registrato!'
 for (let i = 0; i < mailList.length; i++) {
-    if (mailList.at(i) === usermail) {
-        message = `Bentornato ${usermail}`
+    if (mailList.at(i) === userMail) {
+        message = `Bentornato ${userMail}`
     }
 }
 alert(message)
