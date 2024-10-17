9511e6665b1ea2ec77f41026ad3093872215f3e9
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-17 18:39:59 +0200

    ADD: solution with  cycle for to mail snack

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,4 +2,12 @@ const mailList = ['vitafrancesco00@gmail.com', 'romelosurdo@gmail.com', 'stresaa
 console.log(mailList)
 
 const usermail = prompt('Inserisci la tua mail')
-mailList.includes(usermail) ? alert(`Bentornato ${usermail}`) : alert(`Non sei registrato!`) //${usermail} desideri registrarti? (implementare poi)
+// mailList.includes(usermail) ? alert(`Bentornato ${usermail}`) : alert(`Non sei registrato!`) //${usermail} desideri registrarti? (implementare poi)
+
+let  message = 'Non sei registrato!'
+for (let i = 0; i < mailList.length; i++) {
+    if (mailList.at(i) === usermail) {
+        message = `Bentornato ${usermail}`
+    }
+}
+alert(message)
