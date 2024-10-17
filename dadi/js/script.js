ff874c81ae259b4b8b9d0f50e11622c9d87ae15d
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-17 19:07:31 +0200

    ADD: dadi solution with if and if else

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,4 +3,13 @@ let userDice = parseInt((Math.random() * 6) + 1)
 console.log(userDice)
 const computerDice = parseInt((Math.random() * 6) + 1)
 console.log(computerDice)
-userDice > computerDice ? alert(`Hai vinto!`) : (userDice < computerDice ? alert(`Hai perso :/`) : alert(`é un pareggio...`))
+// userDice > computerDice ? alert(`Hai vinto!`) : (userDice < computerDice ? alert(`Hai perso :/`) : alert(`é un pareggio...`))
+
+// metodo con if 
+if (userDice > computerDice) {
+    alert(`Hai vinto!`)
+} else if(userDice < computerDice) {
+    alert(`Hai perso :/`)
+} else {
+    alert(`é un pareggio...`)
+}
